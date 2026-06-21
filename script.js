a5ecd1df59199dd798998ff59e207c8bb9e16f82
Belen Campos <lenny121957869@gmail.com>
2026-06-21 00:08:06 -0500

    made the button funcitonal

--- script.js
+++ script.js
@@ -12,5 +12,13 @@ function addTask(){
         span.innerHTML="\u00d7";
         li.appendChild(span);
     }
-    inputBox.value=="";
+    inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
