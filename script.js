f6f8f1abff6d3daf05a9bdb3949ab8fe1bc40145
Belen Campos <lenny121957869@gmail.com>
2026-06-20 23:54:02 -0500

    fixing wrong quotation in add task fucniton

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const inputBox= document.getElementById('input-box');
-const listContainer= document.getElementById('list-container');
+const inputBox= document.getElementById("input-box");
+const listContainer= document.getElementById("list-container");
+
 function addTask(){
-    if (inputBox.value===''){
-        alert("you must add 4 tasks to begin!");
+    if (inputBox.value === ''){
+        alert("you must add a task to begin!");
     }else{
         let li= document.createElement("li");
         li.innerHTML= inputBox.value;
         listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="/u00d7";
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
         li.appendChild(span);
     }
     inputBox.value=="";
