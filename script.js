7def540fd3032452a47b2ff46ffd292cff31db96
Belen Campos <lenny121957869@gmail.com>
2026-06-22 01:32:19 -0500

    resized images

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const butterflyLifeCycle=[
     "assets/butterfly.png"
 ];
 
-const selectedButterflyImageSRC = butterflyLifeCycle[2];
+const selectedButterflyImageSRC = butterflyLifeCycle[0];
 const container = document.getElementById("butterfly-image-container");
 const imgElement = document.createElement("img");
 imgElement.src = selectedButterflyImageSRC;
