70227a012751c7cebd36b088352aa7c7d06570c9
Belen Campos <lenny121957869@gmail.com>
2026-06-22 01:58:49 -0500

    attempting to fix with copiolit pt 2

--- script.js
+++ script.js
@@ -6,20 +6,18 @@ const butterflyLifeCycle=[
     "assets/chrysalis.png",
     "assets/butterfly.png"
 ];
+let butterflyImageElement = null;
 
 window.addEventListener("load", function() {
     setupButterflyImage();
 });
 
 function setupButterflyImage() {
-    const stage = showButterflyStage4();
-    const selectedButterflyImageSRC = butterflyLifeCycle[stage];
     const container = document.getElementById("butterfly-image-container");
-    const imgElement = document.createElement("img");
-    imgElement.src = selectedButterflyImageSRC;
-    
     if (container) {
-        container.appendChild(imgElement);
+        butterflyImageElement = document.createElement("img");
+        container.appendChild(butterflyImageElement);
+        showButterflyStage4();
     }
 }
 
@@ -69,22 +67,22 @@ function showButterflyStage4(){
     
     if(remaining === 1 ){
         stage = 0;
-        document.body.style.backgroundImage= "url('" + butterflyLifeCycle[0] + "')";
     }
     else if(remaining === 2 ){
         stage = 1;
-        document.body.style.backgroundImage= "url('" + butterflyLifeCycle[1] + "')";
     }
     else if(remaining === 3 ){
         stage = 2;
-        document.body.style.backgroundImage= "url('" + butterflyLifeCycle[2] + "')";
     }
     else if(remaining === 4 ){
         stage = 3;
-        document.body.style.backgroundImage= "url('" + butterflyLifeCycle[3] + "')";
     }else{
         stage = 3;
         showButterflyStageGreaterThan4();
+    }
+    
+    if (butterflyImageElement) {
+        butterflyImageElement.src = butterflyLifeCycle[stage];
     }
     return stage;
 }
