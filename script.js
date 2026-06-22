ad01a7bbc347fa786f8f49651944bc9c598e87ea
Belen Campos <lenny121957869@gmail.com>
2026-06-22 02:13:32 -0500

    deleted what copiolit wrote because i ned to figure this out myself and it also did it wrong

--- script.js
+++ script.js
@@ -6,20 +6,6 @@ const butterflyLifeCycle=[
     "assets/chrysalis.png",
     "assets/butterfly.png"
 ];
-let butterflyImageElement = null;
-
-window.addEventListener("load", function() {
-    setupButterflyImage();
-});
-
-function setupButterflyImage() {
-    const container = document.getElementById("butterfly-image-container");
-    if (container) {
-        butterflyImageElement = document.createElement("img");
-        container.appendChild(butterflyImageElement);
-        showButterflyStage4();
-    }
-}
 
 function addTask(){ 
     if (inputBox.value === ''){
@@ -45,47 +31,4 @@ listContainer.addEventListener("click", function(e){
         showButterflyStage4();
     }
 }, false);
-
-
-function countListItems(){
-    if (!listContainer) return 0;
-    const items = listContainer.getElementsByTagName('li');
-    return items.length;
-}
-
-
-function countCheckedItems(){
-    if (!listContainer) return 0;
-    const checkedItems= listContainer.querySelectorAll('li.checked');
-    return checkedItems.length;
-}
-
-
-function showButterflyStage4(){
-    const remaining = countListItems() - countCheckedItems();
-    let stage = 0;
-    
-    if(remaining === 1 ){
-        stage = 0;
-    }
-    else if(remaining === 2 ){
-        stage = 1;
-    }
-    else if(remaining === 3 ){
-        stage = 2;
-    }
-    else if(remaining === 4 ){
-        stage = 3;
-    }else{
-        stage = 3;
-        showButterflyStageGreaterThan4();
-    }
-    
-    if (butterflyImageElement) {
-        butterflyImageElement.src = butterflyLifeCycle[stage];
-    }
-    return stage;
-}
-
-
 
